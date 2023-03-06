7c24d8c4abb2838543aa40bd640c14369d50c8e0
ak1103dev <ak.apichan1103@gmail.com>
2023-03-06 20:31:28 +0700

    :construction: add tailwind

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -2,8 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function AuthLayout() {
   return (
-    <div>
-      <h1>Auth Layout</h1>
+    <div className="container flex mx-auto my-0">
       <Outlet />
     </div>
   )
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -2,10 +2,12 @@ import { Link } from 'react-router-dom'
 
 export default function Landing() {
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center container">
       <h1>Landing Page</h1>
-      <Link to="/login">Log in</Link>
-      <Link to="/register">Register</Link>
+      <div className="flex justify-between w-60">
+        <Link className="bg-blue-700 text-white" to="/login">Log in</Link>
+        <Link to="/register">Register</Link>
+      </div>
     </div>
   );
 }
